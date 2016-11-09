2e175e5cac24511ad976277d0a4d97d80fe398a9
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2016-11-09 17:20:48 +0530

    First Commit

---
+++ SentimentAnalysisModule/auth.py
@@ -0,0 +1,34 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+from API_DATA import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+def get_stream():
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['#indiaagainstcorruption', '#IndiaAgainstCorruption', '#ModiFightsCorruption', '#modifightscorruption'])
+
+
+if __name__ == '__main__':
+
+    
